40a058d3dec403190ac2ba95e903e163c400513b
morninng <mr_morninng_star_dash@yahoo.co.jp>
2017-04-03 18:19:43 +0900

    get audio data, store it on google storage, recognize the speech is implemented

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const firebase_admin = require("firebase-admin");
 const serviceAccount = require("./secret/mixidea-91a20-firebase-adminsdk.json");
 const config = require('./config/mixidea.conf');
 
-var gcs = require('@google-cloud/storage')({
+const gcs = require('@google-cloud/storage')({
   projectId: 'grape-spaceship-123',
-  keyFilename: './cloud-function-test-192f31cb3070.json'
+  keyFilename: './secret/cloud-function-test-192f31cb3070.json'
 });
- 
 
- var bucket = gcs.bucket('nodetest-moriyama');
+ const bucket = gcs.bucket('nodetest-moriyama');
+ const bucket_path = "gs://nodetest-moriyama/"
  
 
 const serverPort = 3000;
@@ -80,146 +80,85 @@ mixidea_io.on('connection',(socket)=>{
  //   loggerRequest.info("user disconnected socket id=" + socket.id);
   });
 
-	ss(socket).on('audio_record_start', (stream, data)=>{
-			console.log("audio record start socket id=" + socket.id);
-	//		loggerRequest.info("audio record start socket id=" + socket.id);
+	ss(socket).on('record_start_or_resume', (stream, data)=>{
 			stream.setMaxListeners(Infinity);
-			const outfile_name_original  = data.filename;
-			const outfile_name = outfile_name_original.replace(/-/g, "");
       const event_id = data.event_id;
-			const record_start_time = Date.now();
-			console.log("audio record start at " + record_start_time + " socketid:" + socket.id);
-	//		loggerRequest.info("audio record start at " + record_start_time + " socketid:" + socket.id);
+      const role = data.role;
+      const speech_id = data.speech_id;
+      const short_split_id = data.short_split_id;
+			console.log("audio record start socket id=" + socket.id + "event_id: " + event_id );
+      var remoteWriteStream = bucket.file("raw/" + event_id + "/" + role + "__" + speech_id + "/" + short_split_id + '.raw').createWriteStream();
+      stream.pipe(remoteWriteStream);
 
-			const sample_rate = data.sample_rate || 44100;
-			eval("GlobalInfo.file_writer_count_" + outfile_name + "=1");
-			eval("GlobalInfo.record_start_time_" + outfile_name + "=" + record_start_time);
-		//	var outfile_name_wav  = './public/audio/' + outfile_name + "_1.wav";
-			const outfile_name_wav  = './public/audio/' + event_id + "/" + outfile_name + "_1.wav";
-			const outputfile_dir = './public/audio/' + event_id;
-			if(!fs.existsSync(outputfile_dir)){
-				fs.mkdirSync(outputfile_dir);
-			}
-			console.log("output file name is " + outfile_name_wav);
-	//		loggerRequest.info("output file name is " + outfile_name_wav);
-
-			var file_writer = new wav.FileWriter(
-			 outfile_name_wav,
-			 {channels:1,
-			  sampleRate:sample_rate,
-			  bitDepth:16}
-			);
-			stream.pipe(file_writer);
 	});
 
-	ss(socket).on('audio_record_resume', (stream, data)=>{
-			console.log("audio record resume " + socket.id);
-	//		loggerRequest.info("audio record resume " + socket.id);
-
-			const outfile_name_original  = data.filename;
-			const outfile_name = outfile_name_original.replace(/-/g, "");
-			const prev_count = eval("GlobalInfo.file_writer_count_" + outfile_name );
-			if(!prev_count){
-				return;
-			}
-			const next_count = prev_count + 1;
-			console.log("resume count is " + next_count);
-	//		loggerRequest.info("resume count is " + next_count);
-			const event_id = data.event_id;
-			const sample_rate = data.sample_rate || 44100;
-
-			eval("GlobalInfo.file_writer_count_" + outfile_name + "=next_count");
-			var outfile_name_wav  = './public/audio/' + event_id + "/"  +  outfile_name + "_" + String(next_count)  + ".wav";
-			console.log("output file name is " + outfile_name_wav);
-	//		loggerRequest.info("output file name is " + outfile_name_wav);
-
-			var file_writer = new wav.FileWriter(
-				 outfile_name_wav, 
-				 {channels:1,
-				  sampleRate:sample_rate,
-				  bitDepth:16}
-			);
-			stream.pipe(file_writer);
-	});
-
-  socket.on('audio_record_suspend', function(data){
-    console.log("audio suspend " + socket.id);
- //   loggerRequest.info("audio suspend " + socket.id);
-  });
-
-
-  socket.on('audio_record_end', function(data){
+  socket.on('record_suspend', function(data){
     console.log("audio record end socket id=" + socket.id);
- //   loggerRequest.info("audio recording end " + socket.id);
-    const outfile_name_original  = data.filename;
-    const deb_style  = data.deb_style;
-    const outfile_name = outfile_name_original.replace(/-/g, "");
-    const role_name  = data.role_name;
+    execute_recognition(data)
+
+  });
+
+
+  socket.on('record_finish', function(data){
+    console.log("audio record end socket id=" + socket.id);
+    execute_recognition(data);
+    concatenate_audiofile(data);
+  });
+});
+
+
+function execute_recognition(data){
+
     const event_id = data.event_id;
+    const role = data.role;
     const speech_id = data.speech_id;
-    console.log("file name is " + outfile_name);
- //   loggerRequest.info("file name is " + outfile_name);
-    console.log("role name is " + role_name);
- //   loggerRequest.info("role name is " + role_name);
+    const short_split_id = data.short_split_id;
+    const each_speech_duration = Number(data.each_speech_duration);
 
-    const record_start_time = eval("GlobalInfo.record_start_time_" + outfile_name);
-    const audio_record_end_time = Date.now();
-    const record_duration = audio_record_end_time - record_start_time;
-    const count = eval("GlobalInfo.file_writer_count_" + outfile_name );
-    console.log("audio record start " + record_start_time);
- //   loggerRequest.info("audio record start " + record_start_time);
-    console.log("audio record end " + audio_record_end_time);
- //   loggerRequest.info("audio record end " + audio_record_end_time);
-    console.log("recording duration is " + record_duration + " msec");
- //   loggerRequest.info("recording duration is " + record_duration + " msec");
-    console.log("file count is " + count );
- //   loggerRequest.info("file count is " + count );
-    eval(" delete GlobalInfo.file_writer_count_" + outfile_name );
-    eval(" delete GlobalInfo.record_start_time_" + outfile_name );
-    setTimeout(function(){
-      convert_SampleRate_transcode_upload_S3(outfile_name,deb_style, count,  role_name, event_id, speech_id);
+    setTimeout(
+      ()=>{
+        const sample_rate = data.sample_rate || 44100;
+        const outfile_name  = data.filename;
+        const event_id = data.event_id;
+        asyncRecognizeGCS( bucket_path + "raw/" + event_id + "/" + role + "__" + speech_id + "/" + short_split_id + '.raw','LINEAR16',  sample_rate);
+      },
+      10*1000 + each_speech_duration * 2
+    )
+}
 
-    }, record_duration);
-  });
+function concatenate_audiofile(){
+    console.log("concatenate_audiofile");
 
-
-  socket.on('chat_message', function (data) {
-    console.log(data);
-  });
-
-
-});
+}
 
 
 
-/*
+// sample is here.
+// https://github.com/GoogleCloudPlatform/nodejs-docs-samples/tree/master/speech
+
+function asyncRecognizeGCS (gcsUri, encoding, sampleRate) {
+
+  console.log("asyncRecognizeGCS start", gcsUri);
+  const Speech = require('@google-cloud/speech');
+  const speech = Speech();
+  const request = {
+    encoding: encoding,
+    sampleRate: sampleRate
+  };
+
+  speech.startRecognition(gcsUri, request)
+    .then((results) => {
+      const operation = results[0];
+      return operation.promise();
+    })
+    .then((transcription) => {
+      console.log(`Transcription: ${transcription}`);
+      save_transcription();
+    });
+}
 
 
-// Upload a local file to a new file to be created in your bucket. 
-bucket.upload('./resource/test.txt', function(err, file) {
-  if (!err) {
-    // "zebra.jpg" is now in your bucket. 
-    console.log("upload success");
-  }else{
-      console.log("upload failed");
-  }
-});
+function save_transcription(){
+  console.log("save_transcription");
+}
 
-
-bucket.upload('./resource/test2.txt', function(err, file) {
-  if (!err) {
-    // "zebra.jpg" is now in your bucket. 
-    console.log("upload success");
-  }else{
-      console.log("upload failed");
-  }
-});
-
-
-
-
-var localReadStream = fs.createReadStream('./resource/angular2_dist.png');
-var remoteWriteStream = bucket.file('angular2_dist.png').createWriteStream();
-localReadStream.pipe(remoteWriteStream);
-
-*/
